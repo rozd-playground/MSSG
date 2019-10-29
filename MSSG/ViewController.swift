c73a8ed42b121f6dc2ee7604fa357f6998ec16fc
Max Rozdobudko <max.rozdobudko@gmail.com>
2019-10-29 14:41:23 +0200

    Initial commit

---
+++ MSSG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSSG
+//
+//  Created by Max Rozdobudko on 29.10.2019.
+//  Copyright © 2019 Max Rozdobudko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
